96672edcc3ebd3df6335a7a9e32806e16ff97e9e
louismdv <louismdv27@gmail.com>
2024-02-16 09:05:57 +0100

    good

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lmerveil <lmerveil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 16:33:21 by lmerveil          #+#    #+#             */
-/*   Updated: 2024/02/15 11:54:08 by lmerveil         ###   ########.fr       */
+/*   Updated: 2024/02/15 15:35:39 by lmerveil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,8 @@
 
 int	main(int ac, char **av)
 {
-	char	*str;
-	int		i;
 	t_data	game;
 
-	str = av[1];
-	i = 0;
 	if (ac != 2)
 	{
 		write(1, "Wrong input format", 18);
